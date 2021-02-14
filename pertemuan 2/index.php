730e1592e3eaa53c18fdc57e95300ea3d263ab4b
farhanmb02 <farhanmb02@gmail.com>
2021-02-14 21:28:52 +0700

    menyeesaikan pertemuan 2

---
+++ pertemuan 2/index.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Farhan Mohamad Bintang Atoilah
+203040128
+https://github.com/farhanmb02/pw2021_203040128
+pertemuan 2 == 12 february 2021
+mempelajari sintaks php
+*/
+
+?>
+ 
+ <?php
+
+
+// Standar outpup 
+// 1. echo ,print
+// 2. print_r
+// 3. var_dump
+
+
+echo"farhanmb";
+print"joni";
+print_r("maman");
+var_dump("john");
+
+
+// penuisan sintaxt php 
+// 1. php didalam html 
+// 2. html dalam php 
+
+
+
+// VAriabel dan Tipe data 
+// variabel 
+// tidak boleh di awali oleh angka 
+
+// $nama = "farhan";
+// echo "hallo nama saya $nama";
+
+
+
+// Operator 
+// 1. aritmaika
+// +  -  /  %   *
+
+// 2. penggabung string / concanitation
+// . 
+// $nama_depan = "baban";
+// $nama_belakang ="santoso";
+
+// echo $nama_depan . " " . $nama_belakang ;
+
+// 3. Assigment (penugasan)
+// =
+// +=
+// -=
+// /=
+// %=
+// *=
+// .=
+
+// 4. perbandingan 
+// > <  >= <= ==  !=
+
+
+// 5. identitas 
+// ===  !==
+
+// 6. logika 
+// &&  dan
+// ||  or
+// !   not
+
+
+
+
+
+?>
+
+
+
+
+
