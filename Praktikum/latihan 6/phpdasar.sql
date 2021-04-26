a43795ecefc5425a8b3e010296817d8147fb5fb9
farhanmb02 <farhanmb02@gmail.com>
2021-04-26 11:51:08 +0700

    edit db

--- Praktikum/latihan 6/phpdasar.sql
+++ Praktikum/latihan 6/phpdasar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2021 at 10:01 AM
+-- Generation Time: Apr 26, 2021 at 06:49 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `phpdasar`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `gambar` varchar(150) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `no_pegawai` int(11) NOT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `bidang` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`gambar`, `nama`, `no_pegawai`, `email`, `bidang`) VALUES
+('gambar1.jpg', 'Aldo', 102001, 'A@gmail.com', 'Marketing'),
+('gambar2.jpg', 'Wiwin', 102002, 'W@gmail.com', 'Frontend'),
+('gambar3.jpg', 'Wulan', 102003, 'Wu@gmail.com', 'Backend'),
+('gambar4.jpg', 'Sukardi', 102004, 'S@gmail.com', 'Finance'),
+('gambar5.jpg', 'Meimei', 102005, 'M@gmail.com', 'Produksi');
 
 -- --------------------------------------------------------
 
@@ -46,14 +71,47 @@ INSERT INTO `mahasiswa` (`id`, `nrp`, `nama`, `jurusan`, `email`, `gambar`) VALU
 (12, '21212121', 'Farhan Mohamad Bintang Atoilah', 'hukum', 'farhanmb02@gmail.com', 'gambar1.jpg'),
 (13, '212121217', 'sumanto', 'pangan', 'bintang335522@gmail.com', 'gambar4.jpg');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'farhanmb02', '$2y$10$cb5w7zvrDDZVZ6InRY6EOe42nnb6yLPt6EQXebeh/BhMVOKBu7xrK'),
+(2, 'maman', '$2y$10$2Revpy3xWsrexOyslT6nyOOQnkoBQ29fPkd4kdES5TUNcdIB1/PeS'),
+(3, 'jojo', '$2y$10$tPhxIA.Fjqq6wp0O7C3FxOP1il1qEuH/f0ZCo6H9DVllb.HJqBZFW');
+
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`no_pegawai`);
+
+--
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -65,6 +123,12 @@ ALTER TABLE `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
